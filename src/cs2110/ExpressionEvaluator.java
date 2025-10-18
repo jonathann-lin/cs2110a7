352581e8b7614673f49f840710da3e7bbe6a3db1
Henry Ji <hj465@cornell.edu>
2025-10-18 16:39:03 -0400

    e

---
+++ src/cs2110/ExpressionEvaluator.java
@@ -0,0 +1,101 @@
+package cs2110;
+import java.util.Scanner;
+
+public class ExpressionEvaluator {
+
+     // TODO: Update these specs as you build out the functionality of the `evaluate()` method.
+    /**
+     * Evaluates the given well-formed mathematical expression `expr` and returns its value.
+     * Currently, the `evaluate()` method supports:
+     * - Single-digit int literals
+     * - Addition
+     * - Multiplication
+     * - Parentheses
+     */
+    public static int evaluate(String expr) {
+        Stack<Integer> operands = new LinkedStack<>();
+        Stack<Character> operators = new LinkedStack<>(); // invariant: contains only '(', '+', and '*'
+
+        boolean expectingOperator = false; // in infix notation, the first operand comes before an operator
+
+            for (char c : expr.toCharArray()) { // arrays are Iterable, so can be used in enhanced-for loops
+            if (c == '(') {
+                assert !expectingOperator : "'(' cannot follow an operand";
+                operators.push('(');
+            } else if (c == '*') {
+                assert expectingOperator : "'*' must follow an operand, not an operator";
+                while (!operators.isEmpty() && operators.peek() == '*') {
+                    oneStepSimplify(operands, operators);
+                }
+                operators.push('*');
+                expectingOperator = false;
+            } else if (c == '+') {
+                assert expectingOperator : "'+' must follow an operand, not an operator";
+                while (!operators.isEmpty() && (operators.peek() == '*'
+                        || operators.peek() == '+')) {
+                    oneStepSimplify(operands, operators);
+                }
+                operators.push('+');
+                expectingOperator = false;
+            } else if (c == ')') {
+                assert expectingOperator : "')' must follow an operand, not an operator";
+                assert !operators.isEmpty() : "mismatched parentheses, extra ')'";
+                while (operators.peek() != '(') {
+                    oneStepSimplify(operands, operators);
+                    assert !operators.isEmpty() : "mismatched parentheses, extra ')'";
+                }
+                operators.pop(); // remove '('
+            } else { // c is a digit
+                assert c >= '0' && c <= '9' : "expression contains an illegal character";
+                assert !expectingOperator : "digits cannot follow an operand, our evaluator only supports single-digit inputs";
+                operands.push(c - '0'); // convert c to an int and auto-box
+                expectingOperator = true;
+            }
+        }
+
+        assert expectingOperator : "expression must end with an operand, not an operator";
+        while (!operators.isEmpty()) {
+            assert operators.peek() != '(' : "mismatched parentheses, extra '('";
+            oneStepSimplify(operands, operators);
+        }
+
+        // If the above assertions pass, the operands stack should include exactly one value,
+        // the return value. We'll include two assertions to verify this as a sanity check.
+        assert !operands.isEmpty();
+        int result = operands.pop();
+        assert operands.isEmpty();
+        return result;
+    }
+
+    /**
+     * Helper method that partially simplifies the expression by `pop()`ping one operator from the
+     * `operators` stack, `pop()`ping its two operands from the `operands` stack, evaluating the
+     * operator, and then `push()`ing its result onto the `operands` stack. Requires that
+     * `operators.peek()` is '+' or '*' and `operands` includes at least two elements.
+     */
+    private static void oneStepSimplify(Stack<Integer> operands, Stack<Character> operators) {
+        char op = operators.pop();
+        assert op == '+' || op == '*';
+
+        int o2 = operands.pop(); // second operand is higher on stack
+        int o1 = operands.pop();
+        operands.push(op == '+' ? o1 + o2 : o1 * o2);
+    }
+
+
+    /**
+     * A very basic calculator application.
+     */
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) { // repeat indefinitely
+                System.out.print("Enter an expression, or enter \"q\" to quit: ");
+                String expr = in.nextLine();
+                if (expr.equals("q")) {
+                    break; // exit loop
+                }
+                System.out.println("= " + evaluate(expr));
+            }
+        }
+    }
+}
